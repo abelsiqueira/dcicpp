3727363d821d2d92d3c3921fad6f5a3bd0421b7f
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-05-18 22:37:42 -0300

    Restoration will be tested now

--- Src/restoration.cpp
+++ Src/restoration.cpp
@@ -120,7 +120,6 @@ namespace DCI {
       vertID.a[k + i]   = pa[i];
     }
     vertID.nz = k + gtnnz;
-    //Create RHS
     Vector dualResidue(*env);
     pReal rdx = dualResidue.get_doublex();
     Vector primalResidue(*env, nvar + nconI);
@@ -128,171 +127,198 @@ namespace DCI {
     Real one[2] = {1,0}, zero[2] = {0,0};
 
     dualResidue.sdmult(*J, 1, one, zero, *c);
-    for (Int i = 0; i < numUpper; i++)
-      rdx[i] += Cx[upperIndex[i]]*multUpper[i];
-    for (Int i = 0; i < numLower; i++)
-      rdx[i] -= Cx[lowerIndex[i]]*multLower[i];
-
-    for (int i = 0; i < nvar + nconI; i++)
-      rpx[i] = Cx[i]*dx[i] - slack[i];
-
-    vertID.rhs = new int[nvar + nconI + numUpper + numLower];
-    for (int i = 0; i < nvar + nconI; i++)
-      vertID.rhs[i] = -rdx[i];
-    for (int i = 0; i < numUpper; i++) {
-      int j = nvar + nconI + i;
-      vertID.rhs[j] = upper[i] - slack[upperIndex[i]] - sigma*vertMu/multUpper[i] - rpx[upperIndex[i]];
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = nvar + nconI + numUpper + i;
-      vertID.rhs[j] = slack[lowerIndex[i]] - lower[i] - sigma*vertMu/multLower[i] + rpx[lowerIndex[i]];
-    }
-
-    vertID.icntl[0] = -1;
-    vertID.icntl[1] = -1;
-    vertID.icntl[2] = -1;
-    vertID.icntl[3] = 0;
-    vertID.job = 6;
-
-    //Factorize matrix
-    dmumps_c(&vertID);
-
-    Real dStep[nvar + nconI], slackStep[nvar + nconI];
-    Real upperStep[numUpper], lowerStep[numLower];
-
-    for (int i = 0; i < nvar + nconI; i++) {
-      dStep[i] = vertID.rhs[i];
-      slackStep[i] = Cx[i]*dStep[i] + rpx[i];
-    }
-    for (int i = 0; i < numUpper; i++) {
-      int j = nvar + nconI + i;
-      upperStep[i] = vertID.rhs[j];
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = nvar + ncon + numUpper + i;
-      lowerStep[i] = vertID.rhs[j];
-    }
-
-    Real alphaAff = 1.0;
-    innerMu = 0.0;
-    for (int i = 0; i < numUpper; i++) {
-      int j = upperIndex[i];
-      innerMu += (upper[i] - slack[upperIndex[i]])*multUpper[i];
-      if (slackStep[j] > 0)
-        alphaAff = Min(alphaAff, (upper[i] - slack[j])/slackStep[j]);
-      if (upperStep[i] > 0)
-        alphaAff = Min(alphaAff, -multUpper[i]/upperStep[i]);
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = lowerIndex[i];
-      innerMu += (lower[i] - slack[lower[i]])*multLower[i];
-      if (slackStep[j] < 0)
-        alphaAff = Min(alphaAff, (lower[i] - slack[j])/slackStep[j]);
-      if (lowerStep[i] > 0)
-        alphaAff = Min(alphaAff, -multLower[i]/lowerStep[i]);
-    }
-    innerMu /= (numUpper + numLower);
-    
-    Real innerMuAff = 0.0;
-    for (int i = 0; i < numUpper; i++) {
-      int j = upperIndex[i];
-      inneqMuAff += (upper[i] - slack[j] - alphaAff*slackStep[j])*
-                    (multUpper[i] + alphaAff*upperStep[i]);
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = lowerIndex[i];
-      inneqMuAff += (lower[i] - slack[j] - alphaAff*slackStep[j])*
-                    (multLower[i] + alphaAff*lowerStep[i]);
-    }
-    Real sigma = pow(innerMuAff/innerMu, 3);
-
-    for (Int i = 0; i < numUpper; i++)
-      rdx[i] += Cx[upperIndex[i]]*multUpper[i];
-    for (Int i = 0; i < numLower; i++)
-      rdx[i] -= Cx[lowerIndex[i]]*multLower[i];
-
-    for (int i = 0; i < nvar + nconI; i++)
-      rpx[i] = Cx[i]*dx[i] - slack[i];
-
-    vertID.rhs = new int[nvar + nconI + numUpper + numLower];
-    for (int i = 0; i < nvar + nconI; i++)
-      vertID.rhs[i] = -rdx[i];
-    for (int i = 0; i < numUpper; i++) {
-      int j = nvar + nconI + i;
-      vertID.rhs[j] = upper[i] - slack[upperIndex[i]] - 
-        slackStep[upperIndex[i]]*upper[i] -
-        sigma*vertMu/multUpper[i] - rpx[upperIndex[i]];
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = nvar + nconI + numUpper + i;
-      vertID.rhs[j] = slack[lowerIndex[i]] - lower[i] +
-        slackStep[lowerIndex[i]]*lower[i] -
-        sigma*vertMu/multLower[i] + rpx[lowerIndex[i]];
-    }
-
-    vertID.icntl[0] = -1;
-    vertID.icntl[1] = -1;
-    vertID.icntl[2] = -1;
-    vertID.icntl[3] = 0;
-    vertID.job = 6;
-
-    dmumps_c(&vertID);
-
-    for (int i = 0; i < nvar + nconI; i++) {
-      dStep[i] = vertID.rhs[i];
-      slackStep[i] = Cx[i]*dStep[i] + rpx[i];
-    }
-    for (int i = 0; i < numUpper; i++) {
-      int j = nvar + nconI + i;
-      upperStep[i] = vertID.rhs[j];
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = nvar + ncon + numUpper + i;
-      lowerStep[i] = vertID.rhs[j];
-    }
-
-    Real alphaPrimal = 1.0, alphaDual = 1.0;
-    innerMu = 0.0;
-    for (int i = 0; i < numUpper; i++) {
-      int j = upperIndex[i];
-      innerMu += (upper[i] - slack[upperIndex[i]])*multUpper[i];
-      if (slackStep[j] > 0)
-        alphaPrimal = Min(alphaPrimal, (1 - epsmu)*(upper[i] - slack[j])/slackStep[j]);
-      if (upperStep[i] > 0)
-        alphaDual = Min(alphaDual, (epsmu - 1)*multUpper[i]/upperStep[i]);
-    }
-    for (int i = 0; i < numLower; i++) {
-      int j = lowerIndex[i];
-      innerMu += (lower[i] - slack[lower[i]])*multLower[i];
-      if (slackStep[j] < 0)
-        alphaPrimal = Min(alphaPrimal, (1 - epsmu)*(lower[i] - slack[j])/slackStep[j]);
-      if (lowerStep[i] > 0)
-        alphaDual = Min(alphaDual, (epsmu - 1)*multLower[i]/lowerStep[i]);
-    }
-    innerMu /= (numUpper + numLower);
-
-    for (int i = 0; i < nvar; i++) {
-      dx[i] += alphaPrimal*dStep[i];
-      slack[i] += alphaPrimal*slackStep[i];
-      xcx[i] += alphaPrimal*dStep[i];
-    }
-    for (int i = 0; i < nconI; i++) {
-      int j = nvar + i;
-      dx[j] += alphaPrimal*dStep[j];
-      slack[j] += alphaPrimal*slackStep[j];
-      scx[i] += alphaPrimal*dStep[j];
-    }
-    for (int i = 0; i < numUpper; i++) {
-      multUpper[i] += alphaDual*upperStep[i];
-    }
-    for (int i = 0; i < numLower; i++) {
-      multLower[i] += alphaDual*lowerStep[i];
-    }
-
-    call_ccfsg(dciFalse);
-    normc = c->norm();
-
+    //******************************************************
     while ( (innerMu > 1e-9) && (normc > rho) ) {
+      for (Int i = 0; i < numUpper; i++)
+        rdx[i] += Cx[upperIndex[i]]*multUpper[i];
+      for (Int i = 0; i < numLower; i++)
+        rdx[i] -= Cx[lowerIndex[i]]*multLower[i];
+
+      for (int i = 0; i < nvar + nconI; i++)
+        rpx[i] = Cx[i]*dx[i] - slack[i];
+
+      vertID.rhs = new int[nvar + nconI + numUpper + numLower];
+      for (int i = 0; i < nvar + nconI; i++)
+        vertID.rhs[i] = -rdx[i];
+      for (int i = 0; i < numUpper; i++) {
+        int j = nvar + nconI + i;
+        vertID.rhs[j] = upper[i] - slack[upperIndex[i]] - rpx[upperIndex[i]];
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = nvar + nconI + numUpper + i;
+        vertID.rhs[j] = slack[lowerIndex[i]] - lower[i] + rpx[lowerIndex[i]];
+      }
+
+      vertID.icntl[0] = -1;
+      vertID.icntl[1] = -1;
+      vertID.icntl[2] = -1;
+      vertID.icntl[3] = 0;
+      vertID.job = 6;
+
+      //Factorize matrix
+      dmumps_c(&vertID);
+
+      Real dStep[nvar + nconI], slackStep[nvar + nconI];
+      Real upperStep[numUpper], lowerStep[numLower];
+
+      for (int i = 0; i < nvar + nconI; i++) {
+        dStep[i] = vertID.rhs[i];
+        slackStep[i] = Cx[i]*dStep[i] + rpx[i];
+      }
+      for (int i = 0; i < numUpper; i++) {
+        int j = nvar + nconI + i;
+        upperStep[i] = vertID.rhs[j];
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = nvar + ncon + numUpper + i;
+        lowerStep[i] = vertID.rhs[j];
+      }
+
+      Real alphaAff = 1.0;
+      innerMu = 0.0;
+      for (int i = 0; i < numUpper; i++) {
+        int j = upperIndex[i];
+        innerMu += (upper[i] - slack[upperIndex[i]])*multUpper[i];
+        if (slackStep[j] > 0)
+          alphaAff = Min(alphaAff, (upper[i] - slack[j])/slackStep[j]);
+        if (upperStep[i] > 0)
+          alphaAff = Min(alphaAff, -multUpper[i]/upperStep[i]);
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = lowerIndex[i];
+        innerMu += (lower[i] - slack[lower[i]])*multLower[i];
+        if (slackStep[j] < 0)
+          alphaAff = Min(alphaAff, (lower[i] - slack[j])/slackStep[j]);
+        if (lowerStep[i] > 0)
+          alphaAff = Min(alphaAff, -multLower[i]/lowerStep[i]);
+      }
+      innerMu /= (numUpper + numLower);
+      
+      Real innerMuAff = 0.0;
+      for (int i = 0; i < numUpper; i++) {
+        int j = upperIndex[i];
+        inneqMuAff += (upper[i] - slack[j] - alphaAff*slackStep[j])*
+                      (multUpper[i] + alphaAff*upperStep[i]);
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = lowerIndex[i];
+        inneqMuAff += (lower[i] - slack[j] - alphaAff*slackStep[j])*
+                      (multLower[i] + alphaAff*lowerStep[i]);
+      }
+      Real sigma = pow(innerMuAff/innerMu, 3);
+
+      for (Int i = 0; i < numUpper; i++)
+        rdx[i] += Cx[upperIndex[i]]*multUpper[i];
+      for (Int i = 0; i < numLower; i++)
+        rdx[i] -= Cx[lowerIndex[i]]*multLower[i];
+
+      for (int i = 0; i < nvar + nconI; i++)
+        rpx[i] = Cx[i]*dx[i] - slack[i];
+
+      vertID.rhs = new int[nvar + nconI + numUpper + numLower];
+      for (int i = 0; i < nvar + nconI; i++)
+        vertID.rhs[i] = -rdx[i];
+      for (int i = 0; i < numUpper; i++) {
+        int j = nvar + nconI + i;
+        vertID.rhs[j] = upper[i] - slack[upperIndex[i]] - 
+          slackStep[upperIndex[i]]*upper[i] -
+          sigma*vertMu/multUpper[i] - rpx[upperIndex[i]];
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = nvar + nconI + numUpper + i;
+        vertID.rhs[j] = slack[lowerIndex[i]] - lower[i] +
+          slackStep[lowerIndex[i]]*lower[i] -
+          sigma*vertMu/multLower[i] + rpx[lowerIndex[i]];
+      }
+
+      vertID.icntl[0] = -1;
+      vertID.icntl[1] = -1;
+      vertID.icntl[2] = -1;
+      vertID.icntl[3] = 0;
+      vertID.job = 6;
+
+      dmumps_c(&vertID);
+
+      for (int i = 0; i < nvar + nconI; i++) {
+        dStep[i] = vertID.rhs[i];
+        slackStep[i] = Cx[i]*dStep[i] + rpx[i];
+      }
+      for (int i = 0; i < numUpper; i++) {
+        int j = nvar + nconI + i;
+        upperStep[i] = vertID.rhs[j];
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = nvar + ncon + numUpper + i;
+        lowerStep[i] = vertID.rhs[j];
+      }
+
+      Real alphaPrimal = 1.0, alphaDual = 1.0;
+      innerMu = 0.0;
+      for (int i = 0; i < numUpper; i++) {
+        int j = upperIndex[i];
+        innerMu += (upper[i] - slack[upperIndex[i]])*multUpper[i];
+        if (slackStep[j] > 0)
+          alphaPrimal = Min(alphaPrimal, (1 - epsmu)*(upper[i] - slack[j])/slackStep[j]);
+        if (upperStep[i] > 0)
+          alphaDual = Min(alphaDual, (epsmu - 1)*multUpper[i]/upperStep[i]);
+      }
+      for (int i = 0; i < numLower; i++) {
+        int j = lowerIndex[i];
+        innerMu += (lower[i] - slack[lower[i]])*multLower[i];
+        if (slackStep[j] < 0)
+          alphaPrimal = Min(alphaPrimal, (1 - epsmu)*(lower[i] - slack[j])/slackStep[j]);
+        if (lowerStep[i] > 0)
+          alphaDual = Min(alphaDual, (epsmu - 1)*multLower[i]/lowerStep[i]);
+      }
+      innerMu /= (numUpper + numLower);
+
+      for (int i = 0; i < nvar; i++) {
+        dx[i] += alphaPrimal*dStep[i];
+        slack[i] += alphaPrimal*slackStep[i];
+        xcx[i] += alphaPrimal*dStep[i];
+      }
+      for (int i = 0; i < nconI; i++) {
+        int j = nvar + i;
+        dx[j] += alphaPrimal*dStep[j];
+        slack[j] += alphaPrimal*slackStep[j];
+        scx[i] += alphaPrimal*dStep[j];
+      }
+      for (int i = 0; i < numUpper; i++) {
+        multUpper[i] += alphaDual*upperStep[i];
+      }
+      for (int i = 0; i < numLower; i++) {
+        multLower[i] += alphaDual*lowerStep[i];
+      }
+
+      call_ccfsg(dciFalse);
+      normc = c->norm();
+
+      if ( (normc < rho) || (innerMu < 1e-6) )
+        break;
+      
+      for (int i = 0; i < numUpper; i++) {
+        vertID.irn[2*i] = nvar + nconI + i + 1;
+        vertID.jcn[2*i] = i + 1;
+        vertiD.a[2*i]   = matrixC[upperIndex[i]];
+
+        vertID.irn[2*i + 1] = nvar + ncon + i + 1;
+        vertID.jcn[2*i + 1] = nvar + ncon + i + 1;
+        vertID.a[2*i + 1]   = -(upper[i] - slack[upperIndex[i]])/multUpper[i];
+      }
+      for (int i = 0; i < numLower; i++) {
+        vertID.irn[2*i + 2*numUpper] = nvar + nconI + numUpper + i + 1;
+        vertID.jcn[2*i + 2*numUpper] = i + 1;
+        vertiD.a[2*i + 2*numUpper]   = -matrixC[lowerIndex[i]];
+
+        vertID.irn[2*i + 1 + 2*numUpper] = nvar + ncon + numUpper + i + 1;
+        vertID.jcn[2*i + 1 + 2*numUpper] = nvar + ncon + numUpper + i + 1;
+        vertID.a[2*i + 1 + 2*numUpper]   = (lower[i] - slack[lower[i]])/multLower[i];
+      }
+
+      dualResidue = *c;
+      dualResidue.sdmult(*J, 0, one, one, d);
+      dualResidue.sdmult(*J, 1, one, zero, dualResidue);
+
     }
 
     free(vertID.rhs);
