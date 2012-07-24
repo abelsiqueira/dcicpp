5fcec36ad79fb6ccb6882332e59d0402ffbe5a2a
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-07-24 01:23:34 -0300

    Change to new cholmod

--- downall.sh
+++ downall.sh
@@ -3,6 +3,6 @@ wget http://www.cise.ufl.edu/research/sparse/amd/current/AMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/camd/current/CAMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/colamd/current/COLAMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/ccolamd/current/CCOLAMD.tar.gz
-wget http://www.cise.ufl.edu/research/sparse/UFconfig/current/UFconfig.tar.gz
+wget http://www.cise.ufl.edu/research/sparse/SuiteSparse_config/current/SuiteSparse_config.tar.gz
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.0.2.tar.gz
 #wget http://cms.tacc.utexas.edu/fileadmin/images/GotoBLAS2-1.13_bsd.tar.gz
