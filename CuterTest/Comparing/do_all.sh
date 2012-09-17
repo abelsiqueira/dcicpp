e5c2b26bb039bf72ec7d151cb377878846557a35
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2012-09-17 11:38:55 -0300

    Made more comparisons using small MASTSIF

--- CuterTest/Comparing/do_all.sh
+++ CuterTest/Comparing/do_all.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-# Primeiro eh preciso pegar os resultados e criar o ratio
+if [ $# -lt 1 ]
+then
+  echo "ERROR: Forgot the test name"
+  exit 1
+fi
+function color_echo () {
+  echo -e "\033[0;32m"$1"\033[0m"
+}
+
+color_echo "Calculating ratio"
 ./calc_performance_ratio.py $1
-# Agora preciso calcular t
+
+color_echo "Generating list of values for t"
 ./gen_t_list.sh $1.ratio
+
+color_echo "Removing symbolic value for maximum from t"
 sed -i '1d' $1.t_list
+
+color_echo "Obtaining maximum value for graph"
 max=$(echo "scale=4; $(./find_max.py $1.t_list) + 1" | bc)
+
+color_echo "Substituting symbolic value to maximum value"
 sed -i "s/max/${max}/g" $1.ratio
-# Agora gera o .m
+
+color_echo "Generating m code for octave"
 ./gen_perf_func.py $1 > $1.m
-# Executa o .m 
+
+color_echo "Executing octave to obtaing png"
 echo `octave -q --eval "$1"`
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$1.pdf $1.ps
