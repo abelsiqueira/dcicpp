d3f6d89a128ad2a58b472728cf8f410af3b042bc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-06-15 01:17:10 -0300

    Update INSTALL

--- downall.sh
+++ downall.sh
@@ -4,5 +4,5 @@ wget http://www.cise.ufl.edu/research/sparse/camd/current/CAMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/colamd/current/COLAMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/ccolamd/current/CCOLAMD.tar.gz
 wget http://www.cise.ufl.edu/research/sparse/SuiteSparse_config/current/SuiteSparse_config.tar.gz
-wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.0.2.tar.gz
-#wget http://cms.tacc.utexas.edu/fileadmin/images/GotoBLAS2-1.13_bsd.tar.gz
+wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
+wget http://github.com/xianyi/OpenBLAS/tarball/v0.2.9
