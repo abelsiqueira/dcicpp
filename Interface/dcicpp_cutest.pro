c2a178960624e5c4daa117b384db7f8ed47b5869
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-07-03 13:53:27 -0300

    Change to use preprocessor

--- Interface/dcicpp_cutest.pro
+++ Interface/dcicpp_cutest.pro
@@ -5,5 +5,5 @@ export PACKAGE=dcicpp
 export PACKDIR=dcicpp
 export PACK_PRECISION="double"
 export PACK_OBJS=""
-export PACKLIBS="-ldcicpp -lbasematrices -lcholmod -lamd -lcolamd -lccolamd -lcamd -lsuitesparseconfig -lmetis -lopenblas -lgfortran -lgfortranbegin -lpthread -lrt"
+export PACKLIBS="-ldcicpp ${HOME}/libraries/preprocessor/preprocessor.o -lbasematrices -lcholmod -lamd -lcolamd -lccolamd -lcamd -lsuitesparseconfig -lmetis -lopenblas -lgfortran -lgfortranbegin -lpthread -lrt"
 export SPECS=""
--- Interface/dcicppmain_cutest.cpp
+++ Interface/dcicppmain_cutest.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 #include "dci.h"
 extern "C" {
+#include "preprocessor.h"
 #include "cutest.h"
 }
 
 using namespace DCI;
 
+Preprocessor *prep;
+
+void ufn (int *status, int *n, double *x, double *f) {
+  ppUFN(prep, status, n, x, f);
+}
+
+void uofg (int * status, int * n, double * x,
+    double * f, double * g, _Bool * grad) {
+  ppUOFG(prep, status, n, x, f, g, grad);
+}
+
+void uhprod (int * status, int * n, _Bool *
+    goth, double * x, double * vector, double * result) {
+  ppUHPROD(prep, status, n, goth, x, vector, result);
+}
+
+void cfn (int * status, int * n, int * m, double
+    * x, double * f, double * c) {
+  ppCFN(prep, status, n, m, x, f, c);
+}
+
+void cofg (int * status, int * n, double * x,
+    double * f, double * g, _Bool * grad) {
+  ppCOFG(prep, status, n, x, f, g, grad);
+}
+
+void chprod (int * status, int * n, int * m,
+    _Bool * goth, double * x, double * y, double * vector,
+    double * result) {
+  ppCHPROD(prep, status, n, m, goth, x, y, vector, result);
+}
+
+void ccfsg (int * status, int * n, int * m, double
+    * x, double * c, int * nnzj, int * lj, double * Jval, int * Jvar,
+    int * Jfun, _Bool * grad) {
+  ppCCFSG(prep, status, n, m, x, c, nnzj, lj, Jval, Jvar, Jfun, grad);
+}
+
 int MAINENTRY () {
   DCI::Interface dci;
   char fname[10] = "OUTSDIF.d";
   Int nvar = 0, ncon = 0, amax = 0;
-  Int funit = 42, ierr = 0, fout = 6, io_buffer = 11, status;
+  Int funit = 42, ierr = 0, status;
+
+  prep = initializePreprocessor();
 
   FORTRAN_open (&funit, fname, &ierr);
   CUTEST_cdimen (&status, &funit, &nvar, &ncon);
@@ -20,31 +61,31 @@ int MAINENTRY () {
   Real x[nvar], bl[nvar], bu[nvar];
 
   if (ncon == 0) {
-    CUTEST_usetup (&status, &funit, &fout, &io_buffer, &nvar, x, bl, bu);
+    setUncFuncs(prep, CUTEST_usetup, CUTEST_ufn, CUTEST_uofg,
+        CUTEST_uhprod);
+    dci.set_ufn(ufn);
+    dci.set_uofg(uofg);
+    dci.set_uprod(uhprod);
+    dci.set_unames(CUTEST_unames);
 
-    dci.set_uofg (CUTEST_uofg);
-    dci.set_uprod (CUTEST_uhprod);
-    dci.set_ufn (CUTEST_ufn);
-    dci.set_unames (CUTEST_unames);
-
-    dci.unc_setup (nvar, x, bl, bu);
+    runUncSetup(prep, &nvar, x, bl, bu);
+    dci.unc_setup(nvar, x, bl, bu);
   } else {
     Real y[ncon], cl[ncon], cu[ncon];
     Bool equatn[ncon], linear[ncon];
-    Int efirst = 0, lfirst = 0, nvfirst = 0;
 
-    CUTEST_csetup (&status, &funit, &fout, &io_buffer, &nvar, &ncon, x, bl, bu,
-        y, cl, cu, equatn, linear, &efirst, &lfirst, &nvfirst);
+    setConFuncs(prep, CUTEST_csetup, CUTEST_cfn, CUTEST_cofg,
+        CUTEST_chprod, CUTEST_ccfsg);
+    dci.set_cfn(cfn);
+    dci.set_cofg(cofg);
+    dci.set_cprod(chprod);
+    dci.set_ccfsg(ccfsg);
     CUTEST_cdimsj (&status, &amax);
     amax *= 2;
 
-    dci.set_cofg (CUTEST_cofg);
-    dci.set_cprod (CUTEST_chprod);
-    dci.set_cfn (CUTEST_cfn);
-    dci.set_ccfsg (CUTEST_ccfsg);
-    dci.set_ccifg (CUTEST_ccifg);
-    dci.set_cnames (CUTEST_cnames);
+//    dci.set_ccifg (CUTEST_ccifg);
 
+    runConSetup(prep, &nvar, x, bl, bu, &ncon, y, cl, cu, equatn, linear);
     dci.set_linear (ncon, linear);
     dci.set_amax (amax);
     dci.con_setup (nvar, x, bl, bu, ncon, y, cl, cu, equatn);
@@ -69,6 +110,8 @@ int MAINENTRY () {
   std::cout << "Setup time: " << time[0] << std::endl
             << "Execution time: " << time[1] << std::endl;
   FORTRAN_close ((&funit), (&ierr));
+
+  destroyPreprocessor(prep);
 
   return 0;
 
