50b795e5b54184fba0fbded51a17af34ea446226
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-09-06 12:05:32 -0300

    Add more tests to test-cutest

--- test-cutest.sh
+++ test-cutest.sh
@@ -8,13 +8,13 @@ mkdir -p $tmpdir
 cp Tests/dcicpp.spc $tmpdir
 cd $tmpdir
 c=0
-for n in $(seq 1 119)
+for problem in $(ls $MASTSIF/*.SIF)
 do
-  g=$(rundcicpp -D HS$n -lgfortran -lgfortranbegin | grep Converged)
+  g=$(rundcicpp -D $problem -lgfortran -lgfortranbegin | grep Converged)
   if [ ! -z "$g" ]; then
     c=$(($c+1))
   fi
 done
 
 echo "Convergence count: $c"
-[ $c -lt 110 ] && exit 1
+[ $c -lt 700 ] && exit 1
