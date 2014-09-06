a9c1b80710202a1dbbfc149310b5b6e6914ba057
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-09-06 11:37:00 -0300

    Add -lgfortran and -lgfortranbegin to rundcicpp call

--- test-cutest.sh
+++ test-cutest.sh
@@ -10,10 +10,11 @@ cd $tmpdir
 c=0
 for n in $(seq 1 119)
 do
-  g=$(rundcicpp -D HS$n | grep Converged)
+  g=$(rundcicpp -D HS$n -lgfortran -lgfortranbegin | grep Converged)
   if [ ! -z "$g" ]; then
-    c=$[$c+1]
+    c=$(($c+1))
   fi
 done
 
+echo "Convergence count: $c"
 [ $c -lt 110 ] && exit 1
